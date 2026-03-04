d2279b784df124c9b5d55e0d33ad36f30a91d8cc
Marcos Fernandes <fernandesmarcos.programador@gmail.com>
2026-03-04 15:36:52 -0300

    Adiciona Atividade05 e enunciado da Atividade06

--- src/dev/marcosfer/atividades/Atividade05.java
+++ src/dev/marcosfer/atividades/Atividade05.java
@@ -1,22 +1,42 @@
 package dev.marcosfer.atividades;
 
+import java.util.Scanner;
+
 public class Atividade05 {
 	public static void main(String args[]) {
 		/* Desenvolva um programa que permita cadastrar as notas de vários alunos.
-		 * 
 		 * O sistema deverá:
 		 * - Solicitar o nome do aluno;
 		 * - Solicitar duas notas;
 		 * - Calcular a média do aluno;
 		 * - Informar se ele está Aprovado (média >= 7),
 		 *   Recuperação (média entre 5 e 6.9) ou Reprovado (média < 5).
-		 * 
-		 * O programa deverá permitir cadastrar vários alunos,
-		 * perguntando ao final de cada cadastro se deseja continuar.
-		 * 
-		 * Ao encerrar, o sistema deverá exibir:
-		 * - A quantidade total de alunos cadastrados;
-		 * - A média geral da turma.
 		 */
+	
+		Scanner teclado = new Scanner(System.in);
+
+			System.out.print("Qual o nome do aluno ? ");
+			String nomeAluno = teclado.nextLine();
+			
+			System.out.print("Quanto o aluno tirou na primeira prova ? ");
+			double notaAluno1 = teclado.nextDouble();
+			
+			System.out.print("Quanto o aluno tirou na segunda prova ? ");
+			double notaAluno2 = teclado.nextDouble();
+			
+			double media = (notaAluno1 + notaAluno2) / 2;
+			System.out.println("A média do aluno " + nomeAluno + " é " + media);
+			
+			if(media >= 7) {
+				System.out.print("O aluno " + nomeAluno + " está aprovado");
+			} else if(media > 5 && media < 7) {
+				System.out.print("O aluno " + nomeAluno + " está de recuperação");
+			} else {
+				System.out.print("O aluno " + nomeAluno + " está reprovado");
+			}
+		
+		teclado.close();
+
 	}
+	
 }
---
+++ src/dev/marcosfer/atividades/Atividade06.java
@@ -0,0 +1,15 @@
+package dev.marcosfer.atividades;
+
+public class Atividade06 {
+	public static void main(String args[]) {
+		/* Usando o sistema base da Atividade 05, atualize com: 
+		 *   		 
+		 * O programa deverá permitir cadastrar vários alunos,
+		 * perguntando ao final de cada cadastro se deseja continuar.
+		 * 
+		 * Ao encerrar, o sistema deverá exibir:
+		 * - A quantidade total de alunos cadastrados;
+		 * - A média geral da turma.
+		 */
+	}
+}
