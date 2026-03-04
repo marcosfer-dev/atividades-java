a4ba8f7040881e8d9d5998a3ee021718e9fd52e3
Marcos Fernandes <fernandesmarcos.programador@gmail.com>
2026-03-03 23:28:17 -0300

    Adiciona enunciado da atividade 5

---
+++ src/dev/marcosfer/atividades/Atividade05.java
@@ -0,0 +1,22 @@
+package dev.marcosfer.atividades;
+
+public class Atividade05 {
+	public static void main(String args[]) {
+		/* Desenvolva um programa que permita cadastrar as notas de vários alunos.
+		 * 
+		 * O sistema deverá:
+		 * - Solicitar o nome do aluno;
+		 * - Solicitar duas notas;
+		 * - Calcular a média do aluno;
+		 * - Informar se ele está Aprovado (média >= 7),
+		 *   Recuperação (média entre 5 e 6.9) ou Reprovado (média < 5).
+		 * 
+		 * O programa deverá permitir cadastrar vários alunos,
+		 * perguntando ao final de cada cadastro se deseja continuar.
+		 * 
+		 * Ao encerrar, o sistema deverá exibir:
+		 * - A quantidade total de alunos cadastrados;
+		 * - A média geral da turma.
+		 */
+	}
+}
