4d7def9aa47370b52306a8f8ad1fc90bb4d043cc
Marcos Fernandes <fernandesmarcos.programador@gmail.com>
2026-03-11 23:41:49 -0300

    Adiciona atividade 10 POO

---
+++ src/dev/marcosfer/atividades/Atividade10.java
@@ -0,0 +1,20 @@
+package dev.marcosfer.atividades;
+
+public class Atividade10 {
+	public static void main (String args[]) {
+		/* 
+		 * Primeira atividade minha de POO onde crio uma classe Estudos,
+		 * contendo métodos e atributos
+		*/
+		
+		Estudo n1 = new Estudo();
+		n1.tempoDeDuracaoEstudo = 2;
+		n1.quantidadeExercicios = 10;
+		n1.disciplinaDeEstudo = "Matemática";
+		n1.material = "Caderno";
+		
+		n1.status();
+		n1.escrever();
+		n1.terminarEstudo();
+	}
+}
---
+++ src/dev/marcosfer/atividades/Estudo.java
@@ -0,0 +1,32 @@
+package dev.marcosfer.atividades;
+
+public class Estudo {
+	int tempoDeDuracaoEstudo;
+	String disciplinaDeEstudo;
+	int quantidadeExercicios;
+	String material;
+	void status() {
+		System.out.print("Irei estudar " + this.disciplinaDeEstudo
+				+ "\nVou estudar por " + this.tempoDeDuracaoEstudo + " horas"
+				+ "\nE fazer " + this.quantidadeExercicios + " exercícios");
+	}
+	
+	void terminarEstudo() {
+		if(this.tempoDeDuracaoEstudo == 0 && this.quantidadeExercicios == 0) {
+			System.out.print("\nTerminei de estudar");
+		} else {
+			System.out.print("\nEstou estudando");
+		}
+	}
+	
+	void escrever() {
+		if(material.equals("Caderno")) {
+			System.out.print("\nEstou anotando");
+		} else if(material.equals("Computador")) {
+			System.out.print("\nEstou digitando");
+		} else {
+			System.out.print("\nEstou só assistindo a aula");
+		}
+	}
+	
+}
