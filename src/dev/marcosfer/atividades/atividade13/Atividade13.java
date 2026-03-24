c17a2c3a6715a00ec3891a2b59a2d217e41c28dc
Marcos <fernandesmarcos.programador@gmail.com>
2026-03-23 23:39:13 -0300

    Adiciona atividade 13

---
+++ src/dev/marcosfer/atividades/atividade13/Atividade13.java
@@ -0,0 +1,13 @@
+package dev.marcosfer.atividades.atividade13;
+
+public class Atividade13 {
+    public static void main(String args[]) {
+
+        ControleRemoto c = new ControleRemoto();
+        c.ligar();
+        c.abrirMenu();
+        c.maisVolume();
+        c.play();
+        c.fecharMenu();
+    }
+}
